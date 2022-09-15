99341a5b780aae568fe7c17cfc3baa9b2b8f58d9
jiaxuanf <jiaxuanfum@gmail.com>
2022-09-16 03:02:24 +0800

    table and main components up

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -10,8 +10,12 @@ def home(response):
     return render(response, "home.html", props)
 
 def summaryReport(response):
-    props = {}
-    return render(response, "summaryReport.html", props)
+    report_info = [
+        ['SIA Corporate Health Report I', '2021.08.20 08:30', 'Batch A (2021, Jan), Batch B (2021, Mar)'],
+        ['ACME Corporate Health Report II', '2020.12.24 15:32', 'Batch D (2020, Oct), Batch E (2020,Dec)'],
+        ['ACME Corporate Health Report I', '2020.08.24 12.29', 'Batch A (2020, Jan), Batch B (2020, Mar), Batch C (2020, Jul)']
+        ]
+    return render(response, "summaryReport.html", {"report_info": report_info})
 
 def setting(response):
     props = {}
