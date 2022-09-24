1b3fdc3f2eb7b28e1a90f37d5b8597ce16864080
jiaxuanf <jiaxuanfum@gmail.com>
2022-09-25 00:13:50 +0800

    updated referencing design system

--- mysite/main/urls.py
+++ mysite/main/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     #path('base/', views.base, name='base page'), 
     path('', views.home, name='home page'), 
     path('setting/', views.setting, name='settings page'),
-    path('summaryReport/', views.summaryReport, name='summary report page')
+    path('summaryReport/', views.summaryReport, name='summary report page'),
+   path('update_nbatches', views.update_nbatches, name='update-nbatches'),
+   path('summaryReport/get_nbatches', views.get_nbatches, name='get-nbatches'),
+   path('modalPageTwo/', views.modalPageTwo, name='modal page two'),
 ]
--- mysite/main/views.py
+++ mysite/main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+num_batches = 1
 
 # Create your views here.
 def home(response):
@@ -11,12 +12,44 @@ def home(response):
 
 def summaryReport(response):
     report_info = [
-        ['SIA Corporate Health Report I', '2021.08.20 08:30', 'Batch A (2021, Jan), Batch B (2021, Mar)'],
-        ['ACME Corporate Health Report II', '2020.12.24 15:32', 'Batch D (2020, Oct), Batch E (2020,Dec)'],
-        ['ACME Corporate Health Report I', '2020.08.24 12.29', 'Batch A (2020, Jan), Batch B (2020, Mar), Batch C (2020, Jul)']
+        ['SIA Corporate Health Report I',  'Batch A (2021, Jan), Batch B (2021, Mar)','2021.08.20 08:30', 'Error found'],
+        ['ACME Corporate Health Report II', 'Batch D (2020, Oct), Batch E (2020,Dec)', '2020.12.24 15:32', 'Ready' ],
+        ['ACME Corporate Health Report I', 'Batch A (2020, Jan), Batch B (2020, Mar), Batch C (2020, Jul)', '2020.08.24 12.29', 'Ready']
         ]
-    return render(response, "summaryReport.html", {"report_info": report_info})
+    return render(response, "summaryReport.html", 
+    {
+        "report_info": report_info,
+        "nBatches": num_batches
+      
+    })
+
+def update_nbatches(response):
+    # report_info = [
+    #     ['SIA Corporate Health Report I', '2021.08.20 08:30', 'Batch A (2021, Jan), Batch B (2021, Mar)'],
+    #     ['ACME Corporate Health Report II', '2020.12.24 15:32', 'Batch D (2020, Oct), Batch E (2020,Dec)'],
+    #     ['ACME Corporate Health Report I', '2020.08.24 12.29', 'Batch A (2020, Jan), Batch B (2020, Mar), Batch C (2020, Jul)']
+    #     ]
+    if response == 'POST':
+        num_batches += 1
+        print('added')
+        return render(response, "summaryReport.html", 
+        {
+            # "report_info": report_info,
+            "nBatches": num_batches
+        
+        })
+
+def get_nbatches(response):
+    if response == 'GET':
+        return render(response, 'summaryReport.html', {
+            "nBatches" : num_batches
+        })
+    
 
 def setting(response):
     props = {}
     return render(response, "setting.html", props)
+
+def modalPageTwo(response):
+    props = {}
+    return render(response, "modalPageTwo.html", props)
